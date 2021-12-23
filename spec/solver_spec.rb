d5b3ee0c9e87e5dafd2085ca78bd2a2e7a687e6e
Hichami Houssam <hichami.houssam@gmail.com>
2021-12-23 15:47:27 +0100

    writing tests for reverse string method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,8 +1,9 @@
 require_relative '../class/solver'
 
 describe Solver do
+
+    solver = Solver.new
     context "Return factorial of a number" do
-        solver = Solver.new
 
         it 'should return 1' do
             n = 0
@@ -19,4 +20,16 @@ describe Solver do
             expect(solver.factorial(n)).to eq 120
         end
     end
+
+    describe "test for reverse solver method" do
+        it "should be able to reverse a string" do
+            str1 = "hello"
+            str2 = "the world of programming"
+            expect(solver.reverse(str1)).to eq "olleh"
+            expect(solver.reverse(str2)).to eq "gnimmargorp fo dlrow eht"
+        end
+
+    end
+
+
 end
