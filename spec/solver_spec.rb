1c72d020ac6a56358d0e6cd42c2cf86c735a4340
chaw <77142475+chaw-bot@users.noreply.github.com>
2021-12-23 16:09:51 +0200

    Add test for factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,22 @@
+require_relative '../class/solver'
+
+describe Solver do
+    context "Return factorial of a number" do
+        solver = Solver.new
+
+        it 'should return 1' do
+            n = 0
+            expect(solver.factorial(n)).to eq 1
+        end
+
+        it 'should return an exception if integer is negative' do
+            n = -1
+            expect(solver.factorial(n)).to raise_error('n should be a positive integer')
+        end
+        
+        it 'should return the factorial of 5' do
+            n = 5
+            expect(solver.factorial(n)).to eq 120
+        end
+    end
+end
